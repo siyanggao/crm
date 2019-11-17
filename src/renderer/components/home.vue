c9d6c14b7e7c819402534c24d07c185d2633f91c
gaosiyang <443042389@qq.com>
2019-11-17 13:04:21 +0800

    icon

--- src/renderer/components/home.vue
+++ src/renderer/components/home.vue
@@ -1,10 +1,15 @@
 <template>
 <el-container>
-  <el-header >
-    <el-menu :default-active="$route.path" class="el-menu-demo" mode="horizontal" :router="true">
+  <el-header>
+    <el-image
+      style="width: 15rem; height: 100%; margin-right: 2rem"
+      src="static/logo.png"></el-image>
+    <el-menu :default-active="$route.path" class="el-menu-demo" mode="horizontal" :router="true"
+      style="display:inline-block"
+      active-text-color="#409EFF">
       <el-menu-item index="/">客户管理</el-menu-item>
       <el-submenu index="setting">
-        <template slot="title">设置</template>
+        <template slot="title">设  置</template>
         <el-menu-item index="/setting/customer_group">客户分组</el-menu-item>
         <el-menu-item index="/setting/dict">字典</el-menu-item>
       </el-submenu>
